8f32700b4c2c3fb9aecef4e99388e6043aea7fdc
gabrielferreiracas <162204029+gabrielferreiracas@users.noreply.github.com>
2025-04-04 14:48:28 -0300

    Add files via upload

---
+++ cadastro_01.php
@@ -0,0 +1,26 @@
+<div >
+<?php
+//variaveis 
+
+$email = $_GET['email'];
+$senha = $_GET['senha'];
+$nome = $_GET['nome'];
+$idade = $_GET['idade'];
+$sexo = $_GET['sexo'];
+$data_nascimento = $_GET['data_nascimento'];
+
+
+echo "Sua email é ".$altura;
+echo "<BR>";
+echo "Sua senha é ".$altura;
+echo "<BR>";
+echo "Seu nome é ".$nome;
+echo "<BR>";
+echo "Sua idade é ".$idade;
+echo "<BR>";
+echo "Seu sexo é ".$sexo;
+echo "<BR>";
+
+
+?>
+</div>
